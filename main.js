65ab6b8f376641490b98eca7600e42ad1744c266
justinmo128 <j.mo1@share.epsb.ca>
2023-09-11 14:49:53 -0600

    commit this!

--- main.js
+++ main.js
@@ -1,37 +1,48 @@
 // Canvas and graphics context
 let cnv = document.getElementById("canvas");
 let ctx = cnv.getContext("2d");
-cnv.width = 960;
-cnv.height = 540;
+cnv.width = 480;
+cnv.height = 270;
 
 // Global Variables
-// 60 pixels = 30 units
 // 30 units = 1 block
 let gameState = "gameLoop";
-let currentTime = 0;
-let lastTime = 0
-let lastFrameOccurence = 0;
-let timeDiff = 0;
+let keyHeld = false;
 let background = {
-    colour: "#4287f5",
+    // colour: "#4287f5",
+    colour: "white",
 }
 let floor = {
-    colour: "#0548b3",
+    // colour: "#0548b3",
+    colour: "green",
     y: 0
 };
 let player = {
     colour: "red",
     mode: "cube",
-    x: 160,
-    y: 300,
-    xSpeed: 311.284,
+    x: 0,
+    y: 150,
+    xSpeed: 311.284, // units per second
     xSpeedMod: 1,
-    ySpeed: 0
+    gravity: 0.5,
+    yVel: 0
 }
 let camera = {
     x: 0,
     y: 0
 }
+
+class Spike {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.xHitbox = this.x + 10;
+        this.yHitbox = this.y + 5;
+        this.HBwidth = 10;
+        this.HBheight = 20;
+    }
+}
+let spikes = [new Spike(1000, 30), new Spike (1200, 0)]
 
 // Draw Function
 window.addEventListener("load", draw);
@@ -39,36 +50,12 @@ function draw() {
     if (gameState === "gameLoop") {
         drawGame();
     }
-    // Request Animation Frame
-    // requestAnimationFrame(draw);
-    setTimeout(draw, 0);
-    currentTime = performance.now();
-    timeDiff = currentTime - lastFrameOccurence;
-    lastTime = currentTime;
-}
-
-window.addEventListener("load", physics);
-function physics() {
-    moveCamera();
-    applyGravity();
-    movePlayer();
-    setTimeout(physics, 0);
-}
-
-function drawMainComponents() {
-    // FPS
-    ctx.textAlign = "left"
-    let fps = 1000 / (currentTime - lastFrameOccurence);
-    lastFrameOccurence = currentTime;
-    fps = Math.round(fps);
-    ctx.font = "14px Roboto";
-    ctx.fillStyle = "white";
-    ctx.fillText(`FPS: ${fps}`, 10, 20)
+    setTimeout(draw, 50/3);
 }
 
 function drawGame() {
+    moveCamera();
     drawLevelComponents();
-    drawMainComponents();
     drawPlayer();
 }
 
@@ -78,68 +65,88 @@ function drawLevelComponents() {
     ctx.fillRect(0, 0, cnv.width, cnv.height);
     // Floor
     ctx.fillStyle = floor.colour;
-    ctx.fillRect(0, 360, cnv.width, 180);
+    ctx.fillRect(0, 180, cnv.width, 90);
+    // Spikes
+    ctx.fillStyle = "black";
+    for (let i = 0; i < spikes.length; i++) {
+        ctx.fillRect(spikes[i].x - camera.x, camera.y - spikes[i].y, 30, 30);
+    }
+    
 }
 
 function drawPlayer() {
-    if (player.mode = "cube") {
+    if (player.mode == "cube") {
         ctx.fillStyle = player.colour;
-        ctx.fillRect(160, player.y, 60, 60)
+        if (player.x < 90) {
+            ctx.fillRect(player.x, player.y, 30, 30);
+        } else {
+            ctx.fillRect(player.x - camera.x + 90, player.y - camera.y + 150, 30, 30);
+        }
+        
     }
 }
 
+function keyDown() {
+    keyHeld = true;
+    jump();
+}
+function keyUp() {
+    keyHeld = false;
+}
 // Event Listeners
-// Key down handler
 window.addEventListener("keydown", (e) => {
     if (e.key !== "Escape") {
-        keyDown = true;
-        jump();
+        keyDown();
     }
 })
-
-// Key up handler
 window.addEventListener("keyup", (e) => {
     if (e.key !== "Escape") {
-        keyDown = false;
+        keyUp();
     }
 })
-
-// Mouse down handler
 window.addEventListener("mousedown", () => {
-    keyDown = true;
-    jump();
+    keyDown();
+})
+window.addEventListener("mouseup", () => {
+    keyUp();
 })
 
-// Mouse up handler
-window.addEventListener("mouseup", () => {
-    keyDown = true;
-})
+
+window.addEventListener("load", physics);
+function physics() {
+    applyGravity();
+    movePlayer();
+    setTimeout(physics, 50/3);
+}
 
 function applyGravity() {
-    player.ySpeed += 60 / 1000 * timeDiff;
-    player.y += player.ySpeed;
-    if (player.y >= 300) {
-        player.y = 300;
+    player.y += player.yVel;
+    player.yVel += player.gravity;
+    
+    if (player.yVel > 10) {
+        player.yVel = 10;
+    }
+    if (player.y >= 150) {
+        player.y = 150;
+        setTimeout(() => {if (keyHeld) {jump()}}, 10);
     }
 }
 
 function jump() {
     // Jump height 3 blocks
     // Jump length 3.6 blocks
-    if (player.y >= 300) {
-        player.ySpeed = -10;
+    if (player.y >= 150) {
+        player.yVel = -9;
     }
 }
 
 function movePlayer() {
-    player.x += player.xSpeed / 500 * timeDiff;
-    
-    if (player.y !== 300) {
-        console.log((player.x / 60).toFixed(2), (5 - player.y / 60).toFixed(2))
-    }
+    player.x += player.xSpeed / 60;
 }
 
 function moveCamera() {
-    camera.x = player.x + 160;
-    // camera.y = player.y + 300;
+    if (player.x >= 90) {
+        camera.x = player.x;
+    }
+    camera.y = 150;
 }
